9305ef87cb05621b74165f0bbc56de11492a78f0
LeeSiHyeon711 <dltlgus1230@naver.com>
2025-03-08 21:44:49 +0900

    finish: 도서등록 예외처리

--- learnPHP/BookMarket/addBook_Error.php
+++ learnPHP/BookMarket/addBook_Error.php
@@ -3,7 +3,11 @@
 <head>
     <title>도서 등록</title>
     <link href="./resources/css/bootstrap.min.css" rel="stylesheet">
-    <script type="text/javascript" src="./resources/js/validation.js"></script>
+    <style>
+        .error{ color: red; }
+        .required{ color: red; }
+        .success{ color: green; }
+    </style>
 </head>
 <body class="d-flex flex-column h-100">
     <?php
@@ -14,65 +18,78 @@
         <div class="container py-5">
             <div class="p-5 mb-4 bg-body-tertiary rounded-3">
                 <div class="container-fluid py-5">
-                    <h1 class="display-5 fw-bold">도서 등록</h1>
-                    <p class="col-me-8 fs-4">Book Addition</p>
+                    <h1 class="dispplay-5 fw-bold">도서 등록</h1>
+                    <p class="col-md-8 fs-4">Book Addition</p>
                 </div>
             </div>
             <div class="row align-items-md-stretch">
                 <div class="col-md-12">
                     <div class="h-100 p-5">
-                        <form name="newBook" action="./processAddBook.php" method="post" enctype="multipart/form-data">
+                    <form name="newBook" action="./processAddBook.php" method="post" enctype="multipart/form-data">
                             <div class="mb-3 row">
-                                <label class="col-sm-2">도서코드</label>
+                                <label class="col-sm-2">도서코드<sup class="required">*</sup></label>
                                 <div class="col-sm-3">
-                                    <input type="text" id="bookId" name="bookId" class="form-control">
+                                    <input type="text" id="bookId" name="bookId" value="<?php echo $bookId; ?>" class="form-control">
+                                </div>
+                                <div class="col-sm-6">
+                                    <span class="error"><?php echo $bookIdErr; ?></span>
                                 </div>
                             </div>
                             <div class="mb-3 row">
-                                <label class="col-sm-2">도서명</label>
+                                <label class="col-sm-2">도서명<sup class="required">*</sup></label>
                                 <div class="col-sm-3">
-                                    <input type="text" id="name" name="name" class="form-control">
+                                    <input type="text" id="name" name="name" value="<?php echo $name; ?>" class="form-control">
+                                </div>
+                                <div class="col-sm-6">
+                                    <span class="error"><?php echo $nameErr; ?></span>
                                 </div>
                             </div>
                             <div class="mb-3 row">
-                                <label class="col-sm-2">가격</label>
+                                <label class="col-sm-2">가격<sup class="required">*</sup></label>
                                 <div class="col-sm-3">
-                                    <input type="text" id="unitPrice" name="unitPrice" class="form-control">
+                                    <input type="text" id="unitPrice" name="unitPrice" value="<?php echo $unitPrice; ?>" class="form-control">
+                                </div>
+                                <div class="col-sm-6">
+                                    <span class="error"><?php echo $unitPriceErr; ?></span>
                                 </div>
                             </div>
                             <div class="mb-3 row">
                                 <label class="col-sm-2">저자</label>
                                 <div class="col-sm-3">
-                                    <input type="text" name="author" class="form-control">
+                                    <input type="text" name="author" value="<?php echo $author; ?>" class="form-control">
                                 </div>
                             </div>
                             <div class="mb-3 row">
-                                <label class="col-sm-2">상세정보</label>
-                                <div class="col-sm-5">
-                                    <textarea id="description" name="description" cols="50" rows="2" class="form-control" placeholder="100자 이상 적어주세요"></textarea>
+                                <label class="col-sm-2">상세정보<sup class="required">*</sup></label>
+                                <div class="col-sm-3">
+                                    <textarea id="description" name="description" cols="50" rows="2" class="form-control" placeholder="100자 이상 적어주세요"><?php echo $description; ?></textarea>
+                                    <span class="error"><?php echo $descriptionErr; ?></span>
                                 </div>
                             </div>
                             <div class="mb-3 row">
                                 <label class="col-sm-2">분류</label>
                                 <div class="col-sm-3">
-                                    <input type="text" name="category" class="form-control">
+                                    <input type="text" name="category" value="<?php echo $category; ?>" class="form-control">
                                 </div>
                             </div>
                             <div class="mb-3 row">
-                                <label class="col-sm-2">재고수</label>
+                                <label class="col-sm-2">재고수<sup class="required">*</sup></label>
                                 <div class="col-sm-3">
-                                    <input type="text" id="unitsInStock" name="unitsInStock" class="form-control">
+                                    <input type="text" id="unitsInStock" name="unitsInStock" value="<?php echo $unitsInStock; ?>" class="form-control">
+                                </div>
+                                <div class="col-sm-6">
+                                    <span class="error"><?php echo $unitsInStockErr; ?></span>
                                 </div>
                             </div>
                             <div class="mb-3 row">
                                 <label class="col-sm-2">출판일</label>
                                 <div class="col-sm-3">
-                                    <input type="text" name="releaseDate" class="form-control">
+                                    <input type="text" name="releaseDate" value="<?php echo $releaseDate; ?>" class="form-control">
                                 </div>
                             </div>
                             <div class="mb-3 row">
                                 <label class="col-sm-2">상태</label>
-                                <div class="col-sm-5">
+                                <div class="col-sm-3">
                                     <input type="radio" name="condition" value="New" checked> 신규도서
                                     <input type="radio" name="condition" value="Old"> 중고도서
                                     <input type="radio" name="condition" value="EBook"> E-Book
@@ -80,13 +97,15 @@
                             </div>
                             <div class="mb-3 row">
                                 <label class="col-sm-2">이미지</label>
-                                <div class="col-sm-5">
+                                <div class="col-sm-3">
                                     <input type="file" id="bookImage" name="bookImage" class="form-control">
+                                    <span class="error"><?php echo $bookImageErr; ?></span>
                                 </div>
                             </div>
                             <div class="mb-3 row">
                                 <div class="col-sm-offset-2 col-sm-10">
-                                    <input type="button" class="btn btn-primary" value="등록" onclick="CheckAddBook()">
+                                    <input type="submit" class="btn btn-primary" name="submit" value="등록">
+                                    <input type="reset" class="btn btn-secondary" name="reset" value="취소">
                                 </div>
                             </div>
                         </form>
@@ -97,6 +116,6 @@
     </main>
     <?php
     require "./footer.php";
-    ?>  
+    ?>
 </body>
 </html>
